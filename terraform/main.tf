01d57303d9154d7792d485c825b62dd97e8a62c7
Sertac Engin <sertacenginn@gmail.com>
2026-04-22 19:33:34 +0200

    Created a new SSH Key and using it now

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ variable "location" {
 
 resource "hcloud_ssh_key" "default" {
   name       = "ansible-project-key"
-  public_key = trimspace(file(var.ssh_public_key_path))
+  public_key = trimspace(file("~/.ssh/ansible_capgemini.pub"))
 }
 
 # =========================
