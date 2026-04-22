e89e9956eda5fef1694bdcd7d1c25b356d3ec6af
Sertac Engin <sertacenginn@gmail.com>
2026-04-22 04:31:53 +0200

    Modified generate_inventory.py

--- terraform/generate_inventory.py
+++ terraform/generate_inventory.py
@@ -1,57 +1,36 @@
 import json
 import subprocess
-import os
 
-# -----------------------------
-# Terraform Output holen
-# -----------------------------
 result = subprocess.run(
     ["terraform", "output", "-json"],
     capture_output=True,
-    text=True,
-    cwd="."  # wichtig: im terraform folder ausführen
+    text=True
 )
 
 data = json.loads(result.stdout)
 
-servers = data["servers"]["value"]
+master_ip = data["master_ip"]["value"]
+worker_ip = data["worker_ip"]["value"]
 
-web_ip = servers["web"]["ip"]
-app_ip = servers["app"]["ip"]
-
-# -----------------------------
-# Zielpfad für Ansible Inventory
-# -----------------------------
-inventory_path = "../ansible/inventories/production/inventory.yml"
-
-# Ordner erstellen falls nicht existiert
-os.makedirs(os.path.dirname(inventory_path), exist_ok=True)
-
-# -----------------------------
-# Inventory Inhalt
-# -----------------------------
 inventory = f"""
 all:
   vars:
     ansible_user: root
-    ansible_ssh_private_key_file: /home/sertac/.ssh/hetzner_key
+    ansible_ssh_private_key_file: ~/.ssh/hetzner_key
 
   children:
     web:
       hosts:
         web-1:
-          ansible_host: {web_ip}
+          ansible_host: {master_ip}
 
     app:
       hosts:
         app-1:
-          ansible_host: {app_ip}
+          ansible_host: {worker_ip}
 """
 
-# -----------------------------
-# Datei schreiben
-# -----------------------------
-with open(inventory_path, "w") as f:
+with open("inventory.yml", "w") as f:
     f.write(inventory)
 
-print(f"Inventory created at {inventory_path}")
+print("Inventory created!")
